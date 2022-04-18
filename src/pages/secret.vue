e3061c3323323a1c7364f2a37cf23c7bc7ecb0a8
Bryan <11167417+bryanquijano@users.noreply.github.com>
2022-04-18 06:57:31 -0500

    Message timestamp

--- src/pages/secret.vue
+++ src/pages/secret.vue
@@ -20,20 +20,21 @@ onUnmounted(() => {
 
 <template>
   <h1 class="text-6xl font-thin tracking-tighter text-center mt-8 text-white">
-    Cool Chat
+    Bryan's Real-Time Chat App
   </h1>
   <div
-    class="min-h-[500px] w-full mt-8 rounded-lg shadow-2xl flex flex-col justify-between bg-gray-700 text-white"
+    class="min-h-[500px] w-full my-8 rounded-lg shadow-2xl flex flex-col justify-between bg-gray-700 text-white"
   >
-    <ul class="p-4 space-y-2">
+    <ul class="p-4 space-y-4">
       <li v-for="message in messages" :key="message.id">
         <div
-          class="flex justify-between px-4 py-2 rounded-lg"
+          class="flex justify-between px-4 py-4 rounded-lg hover:bg-gray-900"
           :class="user === message.author ? 'bg-gray-500' : 'bg-gray-800'"
         >
           <span>{{ message.text }} </span>
           <span>by {{ message.author }}</span>
         </div>
+        <p class="text-xs text-gray-400">{{ message.createdAt }}</p>
       </li>
     </ul>
     <div class="border-t-2 border-gray-500">
