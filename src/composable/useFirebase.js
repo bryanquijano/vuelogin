622967aa08eaf446a29a7fadff7475d76c228ad6
Bryan <11167417+bryanquijano@users.noreply.github.com>
2022-04-18 06:20:39 -0500

    Added read from firestore

--- src/composable/useFirebase.js
+++ src/composable/useFirebase.js
@@ -1,9 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 import { firebaseConfig } from "../firebaseConfig";
 
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(firebaseApp);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore();
--- src/pages/secret.vue
+++ src/pages/secret.vue
@@ -1,3 +1,16 @@
+<script setup>
+import { onUnmounted, ref } from "vue";
+import useChat from "../composable/useChat";
+
+const { messages, unsubscribe } = useChat();
+
+const newMessage = ref("");
+
+onUnmounted(() => {
+  unsubscribe();
+});
+</script>
+
 <template>
   <h1 class="text-6xl font-thin tracking-tighter text-center mt-8 text-white">
     Cool Chat
@@ -6,10 +19,10 @@
     class="min-h-[500px] w-full mt-8 rounded-lg shadow-2xl flex flex-col justify-between bg-gray-700 text-white"
   >
     <ul class="p-4 space-y-2">
-      <li v-for="n in 5">
+      <li v-for="message in messages" :key="message.id">
         <div class="flex justify-between bg-gray-600 px-4 py-2 rounded-lg">
-          <span>Message itself </span>
-          <span>by Author Name</span>
+          <span>{{ message.text }} </span>
+          <span>by {{ message.author }}</span>
         </div>
       </li>
     </ul>
@@ -18,6 +31,7 @@
         class="w-full p-4 focus:outline-none focus:bg-gray-600 bg-gray-700"
         type="text"
         placeholder="Type a message..."
+        v-model="newMessage"
       />
     </div>
   </div>
