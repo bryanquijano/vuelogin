69de17153365915f05cdad5d0ca8817f2fe831f0
Bryan <11167417+bryanquijano@users.noreply.github.com>
2022-04-18 05:48:46 -0500

    Chat layout

--- src/pages/secret.vue
+++ src/pages/secret.vue
@@ -1,9 +1,24 @@
 <template>
-  <div class="h-screen">
-    <p class="text-white text-xl pt-3">
-      Welcome to the secret page that only authorized users have access to! If
-      you're seeing this without being logged in to the website then that means
-      I messed up somehow... Oops!
-    </p>
+  <h1 class="text-6xl font-thin tracking-tighter text-center mt-8 text-white">
+    Cool Chat
+  </h1>
+  <div
+    class="min-h-[500px] w-full mt-8 rounded-lg shadow-2xl flex flex-col justify-between bg-gray-700 text-white"
+  >
+    <ul class="p-4 space-y-4">
+      <li v-for="n in 5">
+        <div class="flex justify-between bg-gray-600 px-4 py-2 rounded-lg">
+          <span>Message itself </span>
+          <span>by Author Name</span>
+        </div>
+      </li>
+    </ul>
+    <div class="border-t-2 border-gray-500">
+      <input
+        class="w-full p-4 focus:outline-none focus:bg-gray-600 bg-gray-700"
+        type="text"
+        placeholder="Type a message..."
+      />
+    </div>
   </div>
 </template>
